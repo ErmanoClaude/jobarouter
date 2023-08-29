289bd711134804f093c49557f0b10bfa8792ae0c
Ermano <ermano.claude@cix.csi.cuny.edu>
2023-08-29 14:59:52 -0400

    updated for old way of creating routes

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+export default function About() {
+    return (
+        <div className="About">
+            <h1>About Us</h1>
+            <p>
+                Lorem adipisicing nostrud sit ex dolor consequat do eu sint et dolore sit elit incididunt. 
+                Duis cillum occaecat aliqua Lorem elit velit. Et nulla eiusmod id veniam. 
+                Ea proident in consequat esse sint adipisicing voluptate. Consequat aute eiusmod 
+                commodo ut minim anim eu voluptate sit elit. Sint esse occaecat reprehenderit incididunt anim.
+            </p>
+        </div>
+    );
+};
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+export default function Home() {
+    return (
+        <div className="home">
+            <h1>Welcome to Home Page</h1>
+            <p>
+                Lorem adipisicing nostrud sit ex dolor consequat do eu sint et dolore sit elit incididunt. 
+                Duis cillum occaecat aliqua Lorem elit velit. Et nulla eiusmod id veniam. 
+                Ea proident in consequat esse sint adipisicing voluptate. Consequat aute eiusmod 
+                commodo ut minim anim eu voluptate sit elit. Sint esse occaecat reprehenderit incididunt anim.
+            </p>
+        </div>
+    );
+};
